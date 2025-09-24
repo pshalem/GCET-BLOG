c7c5c7c92c16043e777324c81fd7337f0055a289
pshalem <pshalem2005@gmail.com>
2025-09-24 07:32:15 +0530

    Update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -129,7 +129,7 @@ export default function Login() {
       }}
     >
       {/* Overlay */}
-      <div className="absolute inset-0 bg-background/80 backdrop-blur-sm"></div>
+      <div className="absolute inset-0 bg-background/60 backdrop-blur-sm"></div>
       <motion.div
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
