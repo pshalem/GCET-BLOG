dda014ada80ebc93c652511f9f6661cf288e08ce
pshalem <pshalem2005@gmail.com>
2025-09-24 09:55:14 +0530

    Update use-mobile.tsx

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -17,3 +17,26 @@ export function useIsMobile() {
 
   return !!isMobile;
 }
+
+export function useMobileSidebar() {
+  const [isOpen, setIsOpen] = React.useState(false);
+  const isMobile = useIsMobile();
+
+  const open = () => setIsOpen(true);
+  const close = () => setIsOpen(false);
+  const toggle = () => setIsOpen(!isOpen);
+
+  React.useEffect(() => {
+    if (!isMobile) {
+      setIsOpen(false); 
+    }
+  }, [isMobile]);
+
+  return {
+    isOpen: isMobile ? isOpen : true,
+    isMobile,
+    open,
+    close,
+    toggle
+  };
+}
